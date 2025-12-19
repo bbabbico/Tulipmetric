9d8df0ee8f6cb5e4b687d2e66d867417c325412d
wonehgus <wonehgus1234@naver.com>
2025-12-19 21:05:18 +0900

    Initial commit

---
+++ src/main/java/project7/tulipmetric/TulipmetricApplication.java
@@ -0,0 +1,13 @@
+package project7.tulipmetric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TulipmetricApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TulipmetricApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project7/tulipmetric/TulipmetricApplicationTests.java
@@ -0,0 +1,13 @@
+package project7.tulipmetric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TulipmetricApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
